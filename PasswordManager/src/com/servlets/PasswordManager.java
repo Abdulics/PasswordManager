356f6d3d157591649eee761b3ab742b654d76d1b
abdul <abdul@localhost>
2021-02-27 21:58:57 -0600

    Simple commit for a welcome html page.

---
+++ PasswordManager/src/com/servlets/PasswordManager.java
@@ -0,0 +1,47 @@
+package com.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class PasswordManager
+ */
+@WebServlet("/PasswordManager")
+public class PasswordManager extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PasswordManager() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		try {
+			response.sendRedirect("home.html");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ PasswordManager/src/com/util/Info.java
@@ -0,0 +1,6 @@
+package com.util;
+
+public class Info {
+	public String projectName = "passwordManager";
+	public String home = "home.html";
+}
